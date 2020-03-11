0aae996fa8ef4de5ba8abc1967d856d31de37ff3
Mahan Jahromi <mahanjahromi@tcadmins-MacBook-Air.local>
2020-03-11 09:20:21 +0000

    initial commit

--- venv/Aircraft_class.py
+++ venv/Aircraft_class.py
@@ -1,5 +1,10 @@
 # aircraft class
 # attributes: cargo
+class Aircraft():
+    def __init__(self, cargo):
+        self.cargo = cargo
 
 
 # Define a couple of methods, accelerate and break
+
+def accelerate
